aca5c5c304b15c99d76a0332aef67978d80851bd
CJC0512 <ehdchs2006@gmail.com>
2024-02-28 09:16:07 +0900

    chap05 spring-data-jpa 생성 및 템플릿 추가

---
+++ chap05-spring-data-jpa-lecture-source/src/main/java/com/ohgiraffers/springdatajpa/Chap05SpringDataJpaLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap05SpringDataJpaLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap05SpringDataJpaLectureSourceApplication.class, args);
+    }
+
+}
